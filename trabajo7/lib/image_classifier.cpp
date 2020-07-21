09fee0210090ff49a73b66875106f7e53e8e5124
Alan Vieyra <alan.vieyra376@gmail.com>
2020-07-21 13:15:44 -0500

    Update best_hu_moments

--- trabajo7/lib/image_classifier.cpp
+++ trabajo7/lib/image_classifier.cpp
@@ -9,8 +9,8 @@ using namespace cv;
 int8_t img_classiier(Mat img) {
   Mat img_border;
   int8_t predict = -1;
-  double best_hu_moments_c[7] = {1.594433, 0, 8.499806, 9.950693, 0, 0, 0};
-  double best_hu_moments_t[7] = {1.624184, 3.829316, 5.473052, 7.161877, 0, 0, 0};
+  double best_hu_moments_c[7] = {1.594433, 0, 8.499806, 0, 0, 0, 0};
+  double best_hu_moments_t[7] = {1.624184, 0, 5.473052, 0, 0, 0, 0};
 
   get_img_border(img, img_border);
   // imwrite("debug_img_border.png", img_border);
@@ -24,9 +24,9 @@ int8_t img_classiier(Mat img) {
   double distance_c = distance_hu_moments(huMoments, best_hu_moments_c);
   double distance_t = distance_hu_moments(huMoments, best_hu_moments_t);
 
-  // printf("Distancias a los momentos de hu: %f y %f\n", distance_c, distance_t);
+  printf("Distancias a los momentos de hu: %f y %f\n", distance_c, distance_t);
 
-  if (distance_c <= 1 || distance_t <= 1) {
+  if (distance_c <= 0.84 || distance_t <= 0.58) {
     draw_border(img, img_border);
 
     if (distance_c < distance_t) {
@@ -35,9 +35,6 @@ int8_t img_classiier(Mat img) {
       predict = 1;
     }
   }
-
-  // printf("distance_c: %f\n", distance_c);
-  // printf("distance_t: %f\n", distance_t);
 
   return predict;
 }
